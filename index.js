19394fdeeef14afa2cbe9fd693ae4574a2d3dd01
technotronic12 <elip@wix.com>
2018-10-10 10:57:47 +0300

    add hadron

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const path = require('path');
 const bootstrap = require('wix-bootstrap-ng');
+const greynode = require('wix-bootstrap-greynode');
+const hadron = require('wix-bootstrap-hadron');
+
+// required - list the artifacts you want hadron to manage for you
+const opts = {
+  staticArtifacts: [{
+    artifactId: 'com.wixpress.fed.no-hadron-app'
+  }]
+};
 
 const src = process.env.SRC_PATH || path.join('.', 'dist', 'src');
 const server = path.join(src, 'server');
 
 bootstrap()
+  .use(greynode) // required
+  .use(hadron, opts)
   .express(server)
   .start({ disableCluster: process.env.NODE_ENV === 'development' });
