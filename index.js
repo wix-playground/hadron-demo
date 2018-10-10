a5d4d597bea2c630a1dc3781bb7f025ec80e7e65
technotronic12 <elip@wix.com>
2018-10-10 12:16:40 +0300

    Revert "Revert "Revert "add hadron"""
    This reverts commit 689e8ce

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const path = require('path');
 const bootstrap = require('wix-bootstrap-ng');
-const greynode = require('wix-bootstrap-greynode');
-const hadron = require('wix-bootstrap-hadron');
-
-// required - list the artifacts you want hadron to manage for you
-const opts = {
-  staticArtifacts: [{
-    artifactId: 'com.wixpress.no-hadron-app'
-  }]
-};
 
 console.log('yo');
 const src = process.env.SRC_PATH || path.join('.', 'dist', 'src');
 const server = path.join(src, 'server');
 
 bootstrap()
-  .use(greynode) // required
-  .use(hadron, opts)
   .express(server)
   .start({ disableCluster: process.env.NODE_ENV === 'development' });
